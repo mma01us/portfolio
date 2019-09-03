4d4e129e16fc57faa51a70e37fde067bc8fde429
Michael Ma <mma01us@gmail.com>
2019-09-03 18:47:50 -0400

    added better mobile support

--- routes.js
+++ routes.js
@@ -2,32 +2,17 @@
 
 const bodyParser = require("body-parser");
 var path = require("path");
-var mobile = require('is-mobile');
-
-if(mobile())
-    console.log("Mobile User");
-else
-    console.log("Non-mobile User");
 
 module.exports = function(app) {
     app.get("/", function(req, res){ //Index page requested
-        if(mobile())
-            res.render("../src/views/mindex.ejs"); //mobile version
-        else
-            res.render("../src/views/index.ejs"); //Show index page to user
+        res.render("../src/views/index.ejs"); //Show index page to user
     });
 
     app.get("/about", function(req, res){ //About page requested
-        if(mobile())
-            res.render("../src/views/mabout.ejs"); //mobile version
-        else
-            res.render("../src/views/about.ejs"); //Show about page to user
+        res.render("../src/views/about.ejs"); //Show about page to user
     });
 
     app.get("/contact", function(req, res){ //Contact page requested
-        if(mobile())
-            res.render("../src/views/mcontact.ejs"); //mobile version
-        else
-            res.render("../src/views/contact.ejs"); //Show contact page to user
+        res.render("../src/views/contact.ejs"); //Show contact page to user
     });    
 };
