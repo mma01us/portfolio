22f0f129875903a4e1a846f5784ae9a2311b186c
Michael Ma <mma01us@gmail.com>
2019-09-02 23:10:53 -0400

    First version of website

---
+++ routes.js
@@ -0,0 +1,18 @@
+// /routes.js
+
+const bodyParser = require("body-parser");
+var path = require("path");
+
+module.exports = function(app) {
+    app.get("/", function(req, res){ //Index page requested
+        res.render("../src/views/index.ejs"); //Show index page to user
+    });
+
+    app.get("/about", function(req, res){ //About page requested
+        res.render("../src/views/about.ejs"); //Show about page to user
+    });
+
+    app.get("/contact", function(req, res){ //Contact page requested
+        res.render("../src/views/contact.ejs"); //Show contact page to user
+    });    
+};
