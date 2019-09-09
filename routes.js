cc6cab6ff6a13fd5023f0682f275d8f65828ab67
Michael Ma <mma01us@gmail.com>
2019-09-08 22:46:59 -0400

    Updated Creative Challenges.

--- routes.js
+++ routes.js
@@ -8,6 +8,17 @@ module.exports = function(app) {
         res.render("../src/views/index.ejs"); //Show index page to user
     });
 
+    app.get("/projects/:project", function(req, res){
+        console.log(req.params["project"]);
+        switch(req.params["project"]){
+            case '1': //creative challenge
+                res.render("../src/views/creativechallenge.ejs");
+                break;
+            default:
+                res.status(404).send("The requested page could not be found. If you think this is a mistake, contact me at michael.ma@nyu.edu");
+        }
+    });
+
     app.get("/about", function(req, res){ //About page requested
         res.render("../src/views/about.ejs"); //Show about page to user
     });
@@ -15,4 +26,8 @@ module.exports = function(app) {
     app.get("/contact", function(req, res){ //Contact page requested
         res.render("../src/views/contact.ejs"); //Show contact page to user
     });
+
+    app.use(function (req, res, next) {
+        res.status(404).send("The requested page could not be found. If you think this is a mistake, contact me at michael.ma@nyu.edu");
+    })
 };
